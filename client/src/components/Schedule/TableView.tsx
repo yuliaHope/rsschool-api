8af3a676816943f5ea7f8a1f1803b1c4b7b12565
Aleksandr Sharikov <notsaint.mail@gmail.com>
2020-11-12 21:57:25 +0300

    chore: refactor cells renderers

--- client/src/components/Schedule/TableView.tsx
+++ client/src/components/Schedule/TableView.tsx
@@ -1,16 +1,19 @@
-import {
-  QuestionCircleOutlined,
-  YoutubeOutlined,
-  ChromeOutlined,
-  GithubOutlined,
-  SettingOutlined,
-} from '@ant-design/icons';
-import { Table, Tag, Tooltip } from 'antd';
+import { SettingOutlined } from '@ant-design/icons';
+import { Table, Typography } from 'antd';
 import { GithubUserLink } from 'components';
-import { dateSorter, getColumnSearchProps, tagsRenderer } from 'components/Table';
+import {
+  dateSorter,
+  getColumnSearchProps,
+  tagsRenderer,
+  dateWithTimeZoneRenderer,
+  urlRenderer,
+  eventColorTagRenderer,
+  placeRenderer,
+} from 'components/Table';
 import { CourseEvent } from 'services/course';
-import moment from 'moment-timezone';
-import { EventTypeColor, EventTypeToName, TaskTypes } from './model';
+import { TaskTypes } from './model';
+
+const { Text } = Typography;
 
 type Props = {
   data: CourseEvent[];
@@ -23,17 +26,10 @@ export function TableView({ data, timeZone }: Props) {
       rowKey={record => (record.event.type === TaskTypes.deadline ? `${record.id}d` : record.id).toString()}
       pagination={false}
       dataSource={data}
-      // onRow={() => ({
-      //   onClick: ({ target }) => {
-      //     if (!target.closest('a')) {
-      //       // eslint-disable-next-line no-console
-      //       console.log('row is clicked');
-      //     }
-      //   },
-      // })}
+      size="middle"
       columns={[
         {
-          title: columnFilter(),
+          title: <SettingOutlined />,
           width: 20,
           dataIndex: '#',
           render: (_text, _record, index) => index + 1,
@@ -42,85 +38,48 @@ export function TableView({ data, timeZone }: Props) {
           title: 'Date',
           width: 120,
           dataIndex: 'dateTime',
-          render: dateRenderer(timeZone),
+          render: dateWithTimeZoneRenderer(timeZone, 'MMM Do YYYY'),
           sorter: dateSorter('dateTime'),
+          sortDirections: ['descend', 'ascend'],
           defaultSortOrder: 'descend',
         },
-        { title: 'Time', width: 60, dataIndex: 'dateTime', render: timeRenderer(timeZone) },
+        { title: 'Time', width: 60, dataIndex: 'dateTime', render: dateWithTimeZoneRenderer(timeZone, 'HH:mm') },
         {
           title: 'Type',
-          width: 100,
+          width: 120,
           dataIndex: ['event', 'type'],
-          render: (value: keyof typeof EventTypeColor) => (
-            <Tag color={EventTypeColor[value]}>{EventTypeToName[value] || value}</Tag>
-          ),
+          render: eventColorTagRenderer,
         },
         {
           title: 'Special',
           width: 150,
           dataIndex: 'special',
-          render: tags => {
-            if (!tags) {
-              return;
-            }
-            return tagsRenderer(tags.split(','));
-          },
+          render: tags => !!tags && tagsRenderer(tags.split(',')),
         },
         {
           title: 'Name',
           dataIndex: ['event', 'name'],
-          render: (value: string, record) => {
-            return record.event.descriptionUrl ? (
-              <a target="_blank" href={record.event.descriptionUrl}>
-                {value}
-              </a>
-            ) : (
-              value
-            );
-          },
+          render: value => <Text strong>{value}</Text>,
           ...getColumnSearchProps('event.name'),
         },
         {
           title: 'Url',
           width: 30,
-          dataIndex: 'broadcastUrl',
-          render: url => {
-            return url ? (
-              <Tooltip placement="topLeft" title={url}>
-                <a target="_blank" href={url}>
-                  {urlRenderer(url)}
-                </a>
-              </Tooltip>
-            ) : (
-              ''
-            );
-          },
+          dataIndex: ['event', 'descriptionUrl'],
+          render: urlRenderer,
         },
         { title: 'Duration', width: 60, dataIndex: 'duration' },
         {
           title: 'Organizer',
           width: 140,
           dataIndex: ['organizer', 'githubId'],
-          render: (value: string) => (value ? <GithubUserLink value={value} /> : ''),
+          render: (value: string) => !!value && <GithubUserLink value={value} />,
           ...getColumnSearchProps('organizer.githubId'),
         },
         {
           title: 'Place',
           dataIndex: 'place',
-          render: (value: string) => {
-            return value === 'Youtube Live' ? (
-              <div>
-                <YoutubeOutlined /> {value}{' '}
-                <Tooltip title="Ссылка будет в Discord">
-                  <QuestionCircleOutlined />
-                </Tooltip>
-              </div>
-            ) : (
-              <Tooltip title={value}>
-                {<div style={{ textOverflow: 'ellipsis', overflow: 'hidden' }}>{value}</div>}
-              </Tooltip>
-            );
-          },
+          render: placeRenderer,
           onCell: () => {
             return {
               style: {
@@ -134,32 +93,5 @@ export function TableView({ data, timeZone }: Props) {
     />
   );
 }
-
-const dateRenderer = (timeZone: string) => (value: string) =>
-  value ? moment(value, 'YYYY-MM-DD HH:mmZ').tz(timeZone).format('MMM Do YYYY') : '';
-
-const timeRenderer = (timeZone: string) => (value: string) =>
-  value ? moment(value, 'YYYY-MM-DD HH:mmZ').tz(timeZone).format('HH:mm') : '';
-
-const urlRenderer = (url: string) => {
-  const lowerUrl = url.toLowerCase();
-  const isGithubLink = lowerUrl.includes('github');
-  const isYoutubeLink = lowerUrl.includes('youtube');
-  const isYoutubeLink2 = lowerUrl.includes('youtu.be');
-
-  if (isGithubLink) {
-    return <GithubOutlined />;
-  }
-
-  if (isYoutubeLink || isYoutubeLink2) {
-    return <YoutubeOutlined />;
-  }
-
-  return <ChromeOutlined />;
-};
-
-const columnFilter = () => {
-  return <SettingOutlined />;
-};
 
 export default TableView;
