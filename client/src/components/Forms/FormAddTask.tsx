4a69e4daab1baab12cf3e356f040c5cc84b13271
DzmitrySiniakou <jack.pott@mail.ru>
2021-01-29 22:56:23 +0300

    feat: implement new task on cousre

--- client/src/components/Forms/FormAddTask.tsx
+++ client/src/components/Forms/FormAddTask.tsx
@@ -1,18 +1,20 @@
-import React, { useState, useMemo, useCallback } from 'react';
-import { Form, Input, InputNumber, Button, Upload, DatePicker, Select, Spin, Alert, Switch, Drawer } from 'antd';
+import React, { useState, useMemo } from 'react';
+import { useAsync } from 'react-use';
 import { Task, TaskService } from 'services/task';
 import { CourseService, CourseTaskDetails } from 'services/course';
-import { useAsync } from 'react-use';
 import { withSession } from 'components';
 import { Course } from '../../services/models';
 import { UserSearch } from 'components/UserSearch';
 import { UserService } from 'services/user';
-import { UploadOutlined } from '@ant-design/icons';
-// import MapComponent from '../../TaskPageDrawer/Map';
 import { formatTimezoneToUTC } from 'services/formatter';
-import { TIMEZONES } from '../../configs/timezones';
 import DynamicFieldSet from './dmitriSiniakov/DynamicLinksField'
+import { union } from 'lodash';
+import { Form, Input, InputNumber, Button, Upload, DatePicker, Select, Alert } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
 import 'moment-timezone';
+
+import { EVENT_TYPES } from '../../components/Schedule/model';
+import { TIMEZONES } from '../../configs/timezones';
 
 const { Option } = Select;
 const { TextArea } = Input;
@@ -46,13 +48,8 @@ type Props = {
   handleChangeLinks: any,
   onFieldsChange: any,
   onSelectChange: any,
-  // onMapClicked,
-  activeMarker: any,
   loading: any,
   error: any,
-
-  storedTagColors: object;
-  setStoredTagColors: (value: object) => void;
   course: Course;
 }
 
@@ -61,172 +58,143 @@ const FormAddTask: React.FC<Props> = (props) => {
     handleCancel,
     handleChangeLinks,
     onFieldsChange,
-    // onMapClicked,
-    // activeMarker,
-    // loading,
     error,
     course,
   } = props;
-  const courseId = course.id;
-  const service = useMemo(() => new CourseService(courseId), [courseId]);
 
-  const [isMapShown, setMapShown] = useState(false);
-  const [allowFeedback, setAllowFeedback] = useState(false);
-  const [isSuccess, setSuccess] = useState(false);
+  const courseId = course.id;
+  const serviceTask = new TaskService();
   const userService = new UserService();
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState([] as CourseTaskDetails[]);
+  const serviceCouseTask = useMemo(() => new CourseService(courseId), [courseId]);
+  
+  const [isSuccess, setSuccess] = useState(false);
   const [tasks, setTasks] = useState([] as Task[]);
   const [modalData, setModalData] = useState(null as Partial<CourseTaskDetails> | null);
-  const loadData = useCallback(async () => {
-    setLoading(true);
-    const [data, tasks] = await Promise.all([service.getCourseTasksDetails(), new TaskService().getTasks()]);
-    setData(data);
-    setTasks(tasks);
-    setLoading(false);
-  }, [courseId]);
 
-  useAsync(loadData, [courseId]);
+  useAsync(async () => {
+    const tasks = await serviceTask.getTasks();
+    setTasks(tasks);
+  }, [modalData]);
+
+  const allTags = union(...tasks.map(d => d.tags || []));
 
   const loadUsers = async (searchText: string) => {
     return userService.searchUser(searchText);
   };
 
-  const showMap = () => {
-    setMapShown(!isMapShown);
-  };
-
-  const onSwitchChange = async (checked: boolean) => {
-    setAllowFeedback(checked);
-  };
-
-  const handleModalSubmit = (values: any) => { 
+  const handleModalSubmit = async (values: any) => { 
     console.log('before -- ', values);
+    const taskId = await serviceTask.createTask(values);
+
     const [startDate, endDate] = values.range || [null, null];
     values = {
       ...values,
-      courseId: course.id,
+      courseId,
+      taskId: taskId.identifiers[0].id,
       author: values.author ? values.author : 'admin',
-      // map: activeMarker,
       studentStartDate: startDate ? formatTimezoneToUTC(startDate, values.timeZone) : null,
       studentEndDate: endDate ? formatTimezoneToUTC(endDate, values.timeZone) : null,
       descriptionUrl: values.links ? values.links : ['https://www.google.com/'],
-      photo: values.photo ? true : null,
-      video: values.video ? true : null,
-      allowFeedback: allowFeedback,
-      
+      photo: values.photo ? true : null,      
     };
 
     if (!error) {
       setSuccess(true);
     }
     console.log('after -- ', values);
-    service.createCourseTask(values);
+    await serviceCouseTask.createCourseTask(values);
   };
 
-  if (loading) {
-    return <Spin />;
-  }
   if (isSuccess && !error) {
     return <Alert message="Your task successfully added" type="success" showIcon />;
   }
   if (error) {
     return <Alert message="Something went wrong" type="error" showIcon />;
   }
-
-  if (!loading && !error) {
-    return (
-      <Form
-        className="form-add-wrapper"
-        {...layout}
-        name="nest-messages"
-        onFinish={handleModalSubmit}
-        validateMessages={validateMessages}
-        initialValues={{
-          tag: 'self education',
-        }}
-        onFieldsChange={onFieldsChange}
-      >
-        <Form.Item name="taskId" label="Task" rules={[{ required: true, message: 'Please select a task' }]}>
-          <Select showSearch placeholder="Please select a task">
-            {tasks.map((task: Task) => (
-              <Option key={task.id} value={task.id}>
-                {task.name}
-              </Option>
-            ))}
-          </Select>
-        </Form.Item>
-        <Form.Item
-          name="taskOwnerId"
-          label="Task Owner"
-          rules={[{ required: false, message: 'Please select a task owner' }]}
-        >
-          <UserSearch defaultValues={modalData?.taskOwner ? [modalData.taskOwner] : []} searchFn={loadUsers} />
-        </Form.Item>
-        <Form.Item name="timeZone" label="TimeZone">
-          <Select placeholder="Please select a timezone">
-            {TIMEZONES.map(tz => (
-              <Option key={tz} value={tz}>
-                {tz}
-              </Option>
-            ))}
-          </Select>
-        </Form.Item>
-        <Form.Item
-          name="range"
-          label="Start - End Date"
-          rules={[{ required: true, type: 'array', message: 'Please enter start and end date' }]}
-        >
-          <DatePicker.RangePicker format="YYYY-MM-DD HH:mm" showTime={{ format: 'HH:mm' }} />
-        </Form.Item>
-        <Form.Item name="duration" rules={[{ type: 'number', min: 0 }]} label="Duration">
-          <InputNumber min={0} step={0.5} />
-        </Form.Item>
-        <Form.Item name="description" label="Description">
-          <TextArea />
-        </Form.Item>
-        <Form.Item name="result" label="Result">
-          <Input />
-        </Form.Item>
-        <Form.Item name="remark" label="Remark">
-          <Input />
-        </Form.Item>
-        <DynamicFieldSet handleChangeLinks={handleChangeLinks} />   
-        <Form.Item name="map" label="Map">
-          <Button type="dashed" block onClick={showMap}>
-            Show map
-          </Button>
-          {/* {isMapShown && <MapComponent onMapClicked={onMapClicked} activeMarker={activeMarker} darkTheme={darkTheme} />} */}
-        </Form.Item>
-        <Form.Item name="allowFeedback" valuePropName="checked" label="Allow feedback">
-          <Switch defaultChecked onChange={onSwitchChange} />
-        </Form.Item>
-        <Form.Item name="photo" valuePropName="fileList" getValueFromEvent={normFile} label="Photo">
-          <Upload name="logo" action="/upload.do" listType="picture">
-            <Button icon={<UploadOutlined />}>Click to upload</Button>
-          </Upload>
-        </Form.Item>
-        <Form.Item valuePropName="fileList" getValueFromEvent={normFile} label="Video">
-          <Upload name="logo" action="/upload.do" listType="picture">
-            <Button icon={<UploadOutlined />}>Click to upload</Button>
-          </Upload>
-        </Form.Item>
-        <div style={{width: '50%', margin: '0 auto'}}>
-          <Button type="primary" htmlType="submit" style={{margin: '0 10px'}}>
-            Submit
-          </Button>
-          <Button type="default" htmlType="submit" onClick={handleCancel} style={{margin: '0 10px'}}>
-            Cancel
-          </Button>
-        </div>
-      </Form>
-    );
-  }
-
+    
   return (
-    <>
-    </>
-  )
+    <Form
+      className="form-add-wrapper"
+      {...layout}
+      name="nest-messages"
+      onFinish={handleModalSubmit}
+      onChange={values => {
+        setModalData({ ...modalData, checker: values.checker }); // not done
+      }}
+      validateMessages={validateMessages}
+      initialValues={{
+        tag: 'self education',
+      }}    
+      onFieldsChange={onFieldsChange}
+    >
+      <Form.Item name="name" label="Name" rules={[{ required: true, message: 'Please enter task name' }]}>
+        <Input />
+      </Form.Item>
+      <Form.Item name="type" label="Task Type">
+        <Select>
+          {EVENT_TYPES.map(type => <Select.Option value={type} key={type}>{type}</Select.Option>)} 
+        </Select>
+      </Form.Item>
+      <Form.Item
+        name="taskOwnerId"
+        label="Task Owner"
+        rules={[{ required: false, message: 'Please select a task owner' }]}
+      >
+        <UserSearch defaultValues={modalData?.taskOwner ? [modalData.taskOwner] : []} searchFn={loadUsers} />
+      </Form.Item>
+      <Form.Item name="tags" label="Tags">
+        <Select mode="tags">
+          {allTags.map(tag => (
+            <Select.Option key={tag} value={tag}>
+              {tag}
+            </Select.Option>
+          ))}
+        </Select>
+      </Form.Item>
+      <Form.Item name="timeZone" label="TimeZone">
+        <Select placeholder="Please select a timezone">
+          {TIMEZONES.map(tz => (
+            <Option key={tz} value={tz}>
+              {tz}
+            </Option>
+          ))}
+        </Select>
+      </Form.Item>
+      <Form.Item
+        name="range"
+        label="Start - End Date"
+        rules={[{ required: false, type: 'array', message: 'Please enter start and end date' }]}
+      >
+        <DatePicker.RangePicker format="YYYY-MM-DD HH:mm" showTime={{ format: 'HH:mm' }} />
+      </Form.Item>
+      <Form.Item name="duration" rules={[{ type: 'number', min: 0 }]} label="Duration">
+        <InputNumber min={0} step={0.5} />
+      </Form.Item>
+      <Form.Item name="description" label="Description">
+        <TextArea />
+      </Form.Item>
+      <Form.Item name="result" label="Result">
+        <Input />
+      </Form.Item>
+      <Form.Item name="remark" label="Remark">
+        <Input />
+      </Form.Item>
+      <DynamicFieldSet handleChangeLinks={handleChangeLinks} />   
+      <Form.Item name="photo" valuePropName="fileList" getValueFromEvent={normFile} label="Photo">
+        <Upload name="logo" action="/upload.do" listType="picture">
+          <Button icon={<UploadOutlined />}>Click to upload</Button>
+        </Upload>
+      </Form.Item>
+      <div style={{width: '50%', margin: '0 auto'}}>
+        <Button type="primary" htmlType="submit" style={{margin: '0 10px'}}>
+          Submit
+        </Button>
+        <Button type="default" htmlType="submit" onClick={handleCancel} style={{margin: '0 10px'}}>
+          Cancel
+        </Button>
+      </div>
+    </Form>
+  );
 };
 
 export default withSession(FormAddTask);
